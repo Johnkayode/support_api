6628c38f53d5de18eb646385673e6f72319f0dfc
nerdthejohn <newtonjohn043@gmail.com>
2023-11-23 07:52:07 +0100

    chore: trailing slash

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from apps.items.views import ItemAPI
 
 
-router = DefaultRouter(trailing_slash=True)
+router = DefaultRouter(trailing_slash=False)
 router.register("items", ItemAPI, "item")
 
 urlpatterns = [
